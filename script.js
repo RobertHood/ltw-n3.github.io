ab0511a118af928cff2f40377d52e5d8a1e9c4da
100-KG <167777232+100-KG@users.noreply.github.com>
2025-03-11 02:41:17 +0700

    Update script.js - news

--- script.js
+++ script.js
@@ -23,3 +23,62 @@ function changeToLogIn(){
     
 }
 
+function enableComments(){
+    const userId = {
+        name: null,
+        identify: null,
+        image: null,
+        message: null,
+        date: null
+    }
+    
+    const userComment = document.querySelector(".user-cmt");
+    const publishBtn = document.querySelector("#publish");
+    const cmts = document.querySelector(".comments");
+    const userName = document.querySelector(".user");
+    
+    userComment.addEventListener("input", e => {
+        if(!userComment.value){
+            publishBtn.setAttribute("disabled", "disabled");
+            publishBtn.classList.remove("abled");
+        }
+        else{
+            publishBtn.removeAttribute("disabled");
+            publishBtn.classList.add("abled");
+        }
+    })
+
+    function addPost(){
+        console.log("goods");
+        if(!userComment.value) return;
+        userId.name = userName.value;
+        if(userId.name === "Anonymous"){
+            userId.identify = "false";
+            userId.image = "asset/amidisabled.png";
+        }else{
+            userId.identify = true;
+            userId.image = "asset/dinhvanbeo.jpg";
+        }
+        userId.message = userComment.value;
+        userId.date = new Date().toLocaleString();
+        let published = 
+        `<div class = "AppCmts">
+            <img src="${userId.image}">
+            <div>
+                <h1>${userId.name}</h1>
+                <p>${userId.message}</p>
+                <span class="date">${userId.date}</span>
+            </div>
+        </div>`;
+
+
+        let comments = document.querySelector(".comments");
+        comments.innerHTML += published;
+        userComment.value = "";
+
+        let commentsNum = document.querySelectorAll(".AppCmts").length;
+        document.getElementById("comment").textContent = commentsNum;
+    }
+
+    publishBtn.addEventListener("click", addPost)
+}

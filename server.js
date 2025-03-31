25e2fdb69f997d63ba3ac9421b98d7e46b4b302a
Robbie <tranxuanthanhpbe@gmail.com>
2025-04-01 02:04:53 +0700

    mongoDb

--- server.js
+++ server.js
@@ -58,3 +58,27 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
     console.log("Server is Running on http://localhost:3000");
 });
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://robbie1:Boybuster_03@cluster0.gdyalco.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);

a627fa38fee22ea45322244dc1b0cbdd1ae0b6a8
Robbie <tranxuanthanhpbe@gmail.com>
2025-04-04 00:05:48 +0700

    login through MongoDB

---
+++ hasher.js
@@ -0,0 +1,16 @@
+//File này để băm mật khẩu bằng tay nếu như thêm tài khoản qua MongoDB chứ k phải qua màn hình register. Do login băm ngược lại mật khẩu để check nên không lưu password thô trong db như "123" được.
+
+
+const bcrypt = require('bcrypt');
+
+const password = '123';
+
+bcrypt.genSalt(10, (err, salt) => {
+  if (err) throw err;
+
+  bcrypt.hash(password, salt, (err, hash) => {
+    if (err) throw err;
+
+    console.log('Hashed password:', hash);
+  });
+});
--- server.js
+++ server.js
@@ -1,67 +1,11 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
 const path = require("path");
-const server = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, req.url === "/" ? "index.html" : req.url);
-    const extname = path.extname(filePath);
-    let contentType = "text/html";  
-    switch (extname) {
-        case ".css":
-            contentType = "text/css";
-            break;
-        case ".js":
-            contentType = "text/javascript";
-            break;
-        case ".png":
-            contentType = "image/png";
-            break;
-        case ".jpg":
-            contentType = "image/jpg";
-            break;
-        case ".jpeg":
-            contentType = "image/jpeg";
-            break;
-        case ".gif":
-            contentType = "image/gif";
-            break;
-        case ".svg":
-            contentType = "image/svg+xml";
-            break;
-        case ".json":
-            contentType = "application/json";
-            break;
-        default:
-            contentType = "text/html";
-    }
+const bcrypt = require("bcrypt");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-    // Read and serve the file
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            if (err.code === "ENOENT") {
-                // File not found
-                res.writeHead(404, { "Content-Type": "text/plain" });
-                res.end("404 Not Found");
-            } else {
-                // Other server error
-                res.writeHead(500, { "Content-Type": "text/plain" });
-                res.end("Internal Server Error");
-            }
-        } else {
-            // Serve the file with the correct content type
-            res.writeHead(200, { "Content-Type": contentType });
-            res.end(data);
-        }
-    });
-});
-
-// Server listening to port 3000
-server.listen(3000, () => {
-    console.log("Server is Running on http://localhost:3000");
-});
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
 const uri = "mongodb+srv://robbie1:Boybuster_03@cluster0.gdyalco.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -69,16 +13,83 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-async function run() {
+
+let usersCollection;
+
+// Connect to MongoDB
+async function connectDB() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    const db = client.db("Ritogg"); // Change "userDB" to your desired database name
+    usersCollection = db.collection("User");
+    console.log("Connected to MongoDB!");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
   }
 }
-run().catch(console.dir);
+connectDB();
+
+app.use(express.static(path.join(__dirname)));
+app.use(express.json());
+
+// Sign up a user
+app.post("/users", async (req, res) => {
+  try {
+    const { name, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await usersCollection.findOne({ name });
+    if (existingUser) {
+      return res.status(400).send("User already exists");
+    }
+
+    await usersCollection.insertOne({ name, password: hashedPassword });
+    res.status(201).send("User registered successfully!");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error registering user");
+  }
+});
+
+// Log in a user
+app.post("/users/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await usersCollection.findOne({ username });
+
+    if (!user) {
+      return res.status(400).send("User not found");
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch) {
+      res.send("Success");
+    } else {
+      res.status(401).send("Not Allowed");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error logging in");
+  }
+});
+
+// Fetch all users (for testing)
+app.get("/users", async (req, res) => {
+  try {
+    const users = await usersCollection.find().toArray();
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching users");
+  }
+});
+
+// Serve the index.html file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Server listening on port 3000
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});

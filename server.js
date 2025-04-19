692e63f5288faa0f0abb562a2fd28a15590c5c17
Robbie <tranxuanthanhpbe@gmail.com>
2025-04-19 21:47:01 +0700

    change to the thingy

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const cors = require('cors');
 const app = express();
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const path = require('path');
+
 
 const authRouter = require('./routers/authRouter');
 const postsRouter = require('./routers/postsRouter');
-const authController = require('./controllers/authController');
+
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('MongoDB connected successfully!');
@@ -26,8 +30,10 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 app.use('/api/auth', authRouter);
 app.use('/api/posts', postsRouter);
 
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(process.env.PORT, () => {

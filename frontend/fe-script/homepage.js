34f207a167c6bf285d9a59833cfa882cf68da89e
Robbie <tranxuanthanhpbe@gmail.com>
2025-05-08 22:06:18 +0700

    dynamic navbar

--- frontend/fe-script/homepage.js
+++ frontend/fe-script/homepage.js
@@ -1,14 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("lol-carousel").addEventListener("click", () => {
-        window.location.href = "/html/lol-homepage.html";
-    });
-    document.getElementById("val-carousel").addEventListener("click", () => {
-        window.location.href = "/html/valorant-homepage.html";
-    });
-    document.getElementById("tft-carousel").addEventListener("click", () => {
-        window.location.href = "/html/tft-homepage.html";
-    }); 
-    
+document.addEventListener("DOMContentLoaded", () => { 
     const token = getCookie("Authorization");
     
     if (token) {

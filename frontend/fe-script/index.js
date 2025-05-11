eb5fcde89e904752b32e97da572299797b230e81
PhatCT24 <tienphat0812004@gmail.com>
2025-05-11 23:11:28 +0700

    update role in index

--- frontend/fe-script/index.js
+++ frontend/fe-script/index.js
@@ -19,10 +19,16 @@ document.addEventListener("DOMContentLoaded",async () => {
         const userInfo = JSON.parse(atob(token.split('.')[1]));
         document.getElementById("user-info-email").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="none"><g id="SVGRepo_bgCarrier" stroke-width="0"/><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/><g id="SVGRepo_iconCarrier"> <path d="M8 7C9.65685 7 11 5.65685 11 4C11 2.34315 9.65685 1 8 1C6.34315 1 5 2.34315 5 4C5 5.65685 6.34315 7 8 7Z" fill="#FFFFFF"/> <path d="M14 12C14 10.3431 12.6569 9 11 9H5C3.34315 9 2 10.3431 2 12V15H14V12Z" fill="#FFFFFF"/></g></svg>' + userInfo.email;
     }
-    else{
+    else {
         document.getElementById("login-btn").style.display = "block";
         document.getElementById("logout-btn").style.display = "none";
         document.getElementById("user-info").style.display = "none";
+    }
+    if (role === "admin"){
+        document.getElementById("admin-btn").style.display = "block";
+    }
+    else {
+        document.getElementById("admin-btn").style.display ="none";
     }
     document.getElementById("logout-btn").addEventListener("click", async () => {
         try {
